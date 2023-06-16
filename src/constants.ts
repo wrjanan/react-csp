ed1d6f4980583afda0a0c5f3e2abe5a6fc112934
Janan Tan <janan.tan@activate.sg>
2023-06-16 12:36:36 +0800

    add dynamic arguments

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,44 @@
 import { ConfigExtension } from "./types";
 
 let filename = "index.html";
+let path = "/public/";
+let jsPath = "/build/static/js/";
 const args = process.argv.slice(2);
 
-args.forEach(function (value, index) {
+const checkFileNameArg = (value, index) => {
   if(value === '--filename' || value === '--f') {
     if(args[index+1]) {
       filename = args[index+1];
     }
   }
+}
+
+const checkIndexPathArg = (value, index) => {
+  if(value === '--path' || value === '--p') {
+    if(args[index+1]) {
+      path = args[index+1];
+    }
+  }
+}
+
+const checkJSPathArg = (value, index) => {
+  if(value === '--jspath' || value === '--jsp') {
+    if(args[index+1]) {
+      jsPath = args[index+1];
+    }
+  }
+}
+
+args.forEach(function (value, index) {
+  checkFileNameArg(value, index)
+  checkIndexPathArg(value, index)
+  checkJSPathArg(value, index)
 });
 
 const baseDir = process.cwd();
 const configPath = (extension: ConfigExtension) =>
   process.cwd() + `/csp.${extension}`;
-const htmlPath = process.cwd() + `/public/${filename}`;
-const javascriptPath = process.cwd() + `/build/static/js/`;
+const htmlPath = process.cwd() + `${path}${filename}`;
+const javascriptPath = process.cwd() + `${jsPath}`;
 
 export { baseDir, configPath, htmlPath, filename, javascriptPath };
